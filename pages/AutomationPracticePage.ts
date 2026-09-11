2c5802c5ff48bc924a71bc0d8b6934dec4a3b59d
YamilaFogliano <yamilafogliano@hotmail.com>
2026-09-11 11:53:13 -0300

    arreglo de suite de test, restaurando valores

--- pages/AutomationPracticePage.ts
+++ pages/AutomationPracticePage.ts
@@ -150,6 +150,6 @@ export class AutomationPracticePage {
     }
 
     async navigateTo() {
-        await this.page.goto('https://testautomationpractice.blogspot.com/', { waitUntil: 'commit' });
+        await this.page.goto('https://testautomationpractice.blogspot.com/', { waitUntil: 'domcontentloaded' });
     }
 }
